39a56871abe9f42174a1098215c9cc540fa628ad
nchristanto <christanto.nanda@gmail.com>
2019-04-08 20:56:52 +0700

    adding content index.js

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-//for 
+//while loop
+let loop = 0; 
+while (loop <= 20) {
+    console.log(loop)
+    loop++;
+}
 
-//while loops 
+let i = 0; 
+while (i < 5) {
+    console.log('I love nasi padang')
+    i++; 
+}
 
-f
+//for loop 
+for (let j=0; j<6; j++){
+    console.log('Code sleep repeat')
+}
+
+//for loop 
+for (let k=0; k<6; k++){
+    console.log(k)
+}
