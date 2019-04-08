459615195c8d646e54d398db39839c7b839462ee
nchristanto <christanto.nanda@gmail.com>
2019-04-08 21:16:43 +0700

    adding more example in index.js

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-//while loop
+//while loop - example 1
 let loop = 0; 
 while (loop <= 20) {
     console.log(loop)
     loop++;
 }
 
+//example 2
 let i = 0; 
 while (i < 5) {
     console.log('I love nasi padang')
     i++; 
 }
 
-//for loop 
+//for loop- example 3
 for (let j=0; j<6; j++){
     console.log('Code sleep repeat')
 }
 
-//for loop 
+//for loop- example 4
 for (let k=0; k<6; k++){
     console.log(k)
+}
+
+//while loop- example 5
+let iteration = 10
+
+do {
+    console.log(iteration)
+    iteration += 4
+}   while (iteration < 30)
+
+//example 6 
+
+const numbers = [1, 3, 5, 7, 9, 11];
+
+for (let index=0; index < numbers.length; index++){
+    const element = numbers[index]
+    console.log(element)
 }
